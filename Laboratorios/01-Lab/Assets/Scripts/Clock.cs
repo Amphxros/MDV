7911d6e5c5c8700446e08aeee6ed507b55e31551
amprubio <ampxrubio@gmail.com>
2020-10-16 12:46:11 +0200

    Reloj terminado

--- Laboratorios/01-Lab/Assets/Scripts/Clock.cs
+++ Laboratorios/01-Lab/Assets/Scripts/Clock.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform transform_hour;    //Horas
+    public Transform transform_min;     // minutos
+    public Transform transform_sec;     //segundos
+
+    const float DEGREES = -30f;    
     void Start()
     {
-        
+        print("I wanna die");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+       int hour= DateTime.Now.Hour;
+       int min = DateTime.Now.Minute;
+       int sec = DateTime.Now.Second;
+
+      // print(hour +":"+min+ ":"+sec);
+        transform_hour.localRotation = Quaternion.Euler(0, DEGREES * hour,0);
+        transform_min.localRotation = Quaternion.Euler(0, DEGREES/5 * min, 0);
+        transform_sec.localRotation = Quaternion.Euler(0, DEGREES / 5 * sec, 0);
+
     }
 }
