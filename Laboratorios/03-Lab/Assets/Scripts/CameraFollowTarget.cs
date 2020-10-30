0f0d0d3c2d45845db561077a2770c7cbe6013dcb
amprubio <amprubio@ucm.es>
2020-10-30 12:48:39 +0100

    laboratorio 4

---
+++ Laboratorios/03-Lab/Assets/Scripts/CameraFollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowTarget : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    private void Start()
+    {
+        target = target.GetComponent<Transform>();
+        offset = this.transform.position - target.transform.position;
+
+    }
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+            
+     }
+}
---
+++ Laboratorios/03-Lab/Assets/Scripts/InputMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaZ = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(deltaX * speed, 0, deltaZ * speed);
+        mov.Normalize();
+
+        transform.Translate(mov*Time.deltaTime);
+    }
+}
