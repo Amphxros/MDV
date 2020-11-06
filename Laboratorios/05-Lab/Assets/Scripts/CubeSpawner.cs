a3387c99658bb4ac33c57757aa6bfe079d2bcde8
Amph <amprubio@ucm.es>
2020-11-06 13:32:08 +0200

    Laboratorio 5

---
+++ Laboratorios/05-Lab/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate<GameObject>(cube); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
