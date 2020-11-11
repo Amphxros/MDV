04e8dee9ac51f5cee0243423dba05e2e74bcc990
Amph <amprubio@ucm.es>
2020-11-11 13:53:22 +0200

    creado practica 1

---
+++ Practicas/Practica1/Assets/Scripts/Cae.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cae : MonoBehaviour
+{
+    public float units_per_second = 1f;     
+ 
+    void Update()
+    {
+        transform.Translate (new Vector3(0, -units_per_second * Time.deltaTime, 0));
+    }
+}
