e0dcd8edec744452959e673904b5d8c52f5900f6
Amph <amprubio@ucm.es>
2020-12-13 15:09:06 +0200

    creado test 1

--- Practicas/P1/Practica2/Assets/Scripts/PlayerController.cs
+++ Practicas/P1/Practica2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();//pillamos el rigidbody
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");  //pillamos el input horizontal
+        float y = Input.GetAxis("Vertical");    //pillamos el input vertical
+
+        //por defecto si pulsas dos teclas de ejes distintos se movera en el eje X
+        if (x > 0) //movimiento hacia la derecha
+        {
+            transform.up = Vector2.right;
+            rb.velocity = new Vector2(x * speed,0);
+
+        }
+        else if (x < 0)//movimiento hacia la izquierda
+        {
+            transform.up = Vector2.left;
+            rb.velocity = new Vector2( x * speed,0);
+
+        }
+        else if (y < 0)//movimiento hacia abajo
+        {
+            transform.up = Vector2.down;
+            rb.velocity = new Vector2(0, y * speed);
+
+        }
+        else if (y > 0)//movimiento hacia arriba
+        {
+            transform.up = Vector2.up;
+            rb.velocity = new Vector2(0, y*speed);
+        }
+
     }
 }
