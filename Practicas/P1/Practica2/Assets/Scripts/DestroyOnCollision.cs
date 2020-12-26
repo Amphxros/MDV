44c2fc6e78b0e105eceeaa7bb0316c9ae993d891
Amph <amprubio@ucm.es>
2020-12-26 19:27:26 +0200

    Tengo que revisar colliders xd

---
+++ Practicas/P1/Practica2/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+   void OnCollision2DEnter(Collision2D col)
+    {
+        Destroy(this.gameObject);
+    }
+}
