668a46d26247c2a6b5cd61635d94d66ecdf0c92a
Amph <amprubio@ucm.es>
2020-12-26 19:46:07 +0200

    Ahora sí

--- Practicas/P1/Practica2/Assets/Scripts/Destructible.cs
+++ Practicas/P1/Practica2/Assets/Scripts/Destructible.cs
@@ -8,6 +8,6 @@ public class Destructible : MonoBehaviour
     void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.CompareTag("Bullet"))
-        Destroy(this.gameObject);
+            Destroy(this.gameObject);
     }
 }
