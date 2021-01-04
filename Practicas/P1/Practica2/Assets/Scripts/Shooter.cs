9025cca0b2f90975cb1de5baadd1247e41650907
Amph <amprubio@ucm.es>
2021-01-05 00:12:19 +0200

    Shooter definitivo

--- Practicas/P1/Practica2/Assets/Scripts/Shooter.cs
+++ Practicas/P1/Practica2/Assets/Scripts/Shooter.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public GameObject prefab;
-    public float coolingDownSecs; //coolingDownSecs
+    public bool autoshoot = false;
+    public float coolingDownSecs, shootCadenceSecs; //coolingDownSecs
+    
+
     private Transform tr_; //transform del padre
     private float time_;
     void Start()
     {
         time_ = coolingDownSecs;
         tr_ = GetComponentInParent<Transform>(); // obtenemos el transform del objeto padre
-        Shoot(); //esto es para pruebas de momento
     }
    void Update()
    {
         time_ -= Time.time* Time.deltaTime;
+        if(autoshoot)
+            Invoke("Shoot",shootCadenceSecs); //esto es para pruebas de momento
 
    }
+    void OnDestroy()
+    {
+        CancelInvoke();
+    }
    public void Shoot()
    {
         if (time_ <= 0)
