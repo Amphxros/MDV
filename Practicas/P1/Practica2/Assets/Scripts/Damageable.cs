d4b1e9ba9148a20ac2a981962f7e317e89bce93e
Amph <amprubio@ucm.es>
2021-01-06 19:34:43 +0200

    Escena 4 completa???

---
+++ Practicas/P1/Practica2/Assets/Scripts/Damageable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public int max_damage;
+
+    private int current_damage;
+
+    void Start()
+    {
+        current_damage = max_damage;
+    }
+
+    public void MakeDamage()
+    {
+        current_damage--;
+    }
+    public int getDamage() { return current_damage; }
+}
--- Practicas/P1/Practica2/Assets/Scripts/Destructible.cs
+++ Practicas/P1/Practica2/Assets/Scripts/Destructible.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
-
+   
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.CompareTag("Bullet"))
-            Destroy(this.gameObject);
+        if (col.gameObject.CompareTag("Bullet"))
+        {
+            Damageable d = this.gameObject.GetComponent<Damageable>();
+            if (d!=null)
+            {
+                d.MakeDamage();
+                if (d.getDamage() <= 0) //si es un enemigo con 0 de vida o es un bloque (no tienen vida asi que d=null)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+            
+        }
     }
 }
